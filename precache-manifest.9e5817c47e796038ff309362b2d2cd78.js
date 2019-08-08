6a8b205f5ddf334a4504e5a94af43a37ab2754be
Gustavo <gustavoalves.inf@gmail.com>
2019-08-08 11:05:00 -0300

    Updates

---
+++ precache-manifest.9e5817c47e796038ff309362b2d2cd78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e963cd3872c8beeaf3fa4f602b2682",
+    "url": "/calculadora/index.html"
+  },
+  {
+    "revision": "ddc752f5c5cf39e5daf4",
+    "url": "/calculadora/static/css/main.68a92b6f.chunk.css"
+  },
+  {
+    "revision": "efc3d230fd1e1b3caa34",
+    "url": "/calculadora/static/js/2.bbcd3f81.chunk.js"
+  },
+  {
+    "revision": "ddc752f5c5cf39e5daf4",
+    "url": "/calculadora/static/js/main.4cdcaa3e.chunk.js"
+  },
+  {
+    "revision": "7d5cd5d4ee302715cff0",
+    "url": "/calculadora/static/js/runtime~main.3983cfd1.js"
+  }
+]);
